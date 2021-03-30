db9daff3fa95c4e8c825c475ffb16f51a90eede9
punyapd <dahalpp55@gmail.com>
2021-03-30 14:14:59 +0545

    first commit

---
+++ alieninvasion.py
@@ -0,0 +1,97 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+from alien import Alien
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        
+
+
+        
+    def run_game(self):
+        while True:
+            self.create_fleet()
+            self.check_events()
+            self.ship.update()
+            self.bullets.update()
+            self.update_bullets()
+            self._update_aliens()
+            self.update_screen()
+            
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self.check_keydown_events(event)  
+            elif event.type == pygame.KEYUP:
+                self.check_keyup_events(event)
+            
+    def check_keydown_events(self,event):
+        if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+    def check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    def update_bullets(self):
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom < 0 :
+                self.bullets.remove(bullet)
+            
+    def create_fleet(self):
+        alien = Alien(self)
+        alien_width , alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width  - (2 * alien_width)
+        no_of_alien_x = available_space_x // (2 * alien_width)
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - (3* alien_height)- ship_height
+        number_rows = available_space_y//(2*alien_height)
+        for row_number in range(number_rows):
+            for i in range(no_of_alien_x):
+                self._create_alien(i, row_number)
+    def _create_alien(self, i , row_number):
+        alien = Alien(self)
+        alien_width , alien_height = alien.rect.size
+        alien.x = alien_width + 2*alien_width * i
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2* alien.rect.height *row_number
+        self.aliens.add(alien)
+        
+    def update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+        pygame.display.update()
+
+    def _update_aliens(self):
+        self.aliens.update()
+        
+        
+
+if __name__ == '__main__':
+    A = AlienInvasion()
+    A.run_game()
---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 850
+        self.screen_height = 500
+        self.bg_color= (230 ,230,230)
+        self.ship_speed  = 10
+        self.bullet_speed = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 0 ,0)
+        self.bullets_allowed = 5
+        self.alien_speed = 3
+
+    
